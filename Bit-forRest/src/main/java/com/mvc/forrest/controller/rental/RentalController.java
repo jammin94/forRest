038a5be050f1e18fe022c2d0762e5f9c958b7e45
hyeongsan <sanstory12rt@naver.com>
2022-05-31 16:57:06 +0900

    0531 1656 hyeongsan / 물품대여 COMMIT

---
+++ Bit-forRest/src/main/java/com/mvc/forrest/controller/rental/RentalController.java
@@ -0,0 +1,26 @@
+package com.mvc.forrest.controller.rental;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+//==> ȸ������ Controller
+//@Controller
+//@RequestMapping("/user/*")
+public class RentalController {
+
+}
---
+++ Bit-forRest/src/main/java/com/mvc/forrest/dao/rentalreview/RentalReviewDAO.java
@@ -0,0 +1,10 @@
+package com.mvc.forrest.dao.rentalreview;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public class RentalReviewDAO {
+
+}
