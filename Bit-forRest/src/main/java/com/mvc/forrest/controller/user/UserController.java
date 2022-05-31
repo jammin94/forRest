493edfadd6c25d7ecf9a67781e155ca1eef12f3d
hyeongsan <sanstory12rt@naver.com>
2022-05-31 20:21:03 +0900

    0531 2018 hyeongsan / 물품대여 controller 수정

--- Bit-forRest/src/main/java/com/mvc/forrest/controller/user/UserController.java
+++ Bit-forRest/src/main/java/com/mvc/forrest/controller/user/UserController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-//가나다라마바사ㅇㅇㅇ
-
 //==> ȸ������ Controller
 //@Controller
 //@RequestMapping("/user/*")
