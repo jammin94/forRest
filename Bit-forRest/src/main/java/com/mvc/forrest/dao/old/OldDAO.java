8cc789b0d49a502de6764020016437f9b9d48b0a
MS <MS@DESKTOP-ON5VBFQ>
2022-06-01 18:48:02 +0900

    0601 1847 김명선 OldDAO 추가

--- Bit-forRest/src/main/java/com/mvc/forrest/dao/old/OldDAO.java
+++ Bit-forRest/src/main/java/com/mvc/forrest/dao/old/OldDAO.java
@@ -1,5 +1,26 @@
 package com.mvc.forrest.dao.old;
 
-public class oldDAO {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import com.mvc.forrest.service.domain.Old;
+
+@Repository
+@Mapper
+public interface OldDAO {
+
+	public void addOld(Old old) throws Exception ;
+	
+	public Old getOld(int OldNo) throws Exception ;
+	
+	public void updateOld(Old Old) throws Exception ;
+	
+	public void deleteOld(int OldNo) throws Exception ;
+	
+	public List<Old> getOldList() throws Exception ;
+	
+	public int getTotalCount() throws Exception ; 
 }
+//s
