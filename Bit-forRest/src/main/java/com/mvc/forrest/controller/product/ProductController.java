a9e2412f9719f45193811c45e0bb15691c9ade0a
goosebeomsu <captain9697@naver.com>
2022-06-02 20:06:57 +0900

    0602 2006 goosebeomsu: Controller 메서드 추가

--- Bit-forRest/src/main/java/com/mvc/forrest/controller/product/ProductController.java
+++ Bit-forRest/src/main/java/com/mvc/forrest/controller/product/ProductController.java
@@ -12,11 +12,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mvc.forrest.service.domain.Page;
 import com.mvc.forrest.service.domain.Product;
+import com.mvc.forrest.service.domain.Search;
 import com.mvc.forrest.service.product.ProductService;
 
 
@@ -30,30 +33,78 @@ public class ProductController {
 	@Autowired
 	public ProductService productService;
 	
-	//@GetMapping("/addProductTest")
-	public String addProductTest() {
-		return "storage/addStorage";
+	public ProductController() {
+		System.out.println(this.getClass());
 	}
 	
-	//@RequestMapping("addProduct")
-	public String addProduct(@ModelAttribute("product") Product product) throws Exception {
+	@Value("5")
+	int pageUnit;
+	
+	@Value("10")
+	int pageSize;
+	
+	
+	
 		
-		product.setWidth(2);
-		product.setLength(2);
-		product.setHeight(2);
-		product.setUserId("user05@naver.com");
-		product.setProdName("test");
-		product.setProdQuantity(1);
-		product.setProdDetail("굳~~");
-		product.setCategory("텐트");
-		product.setReturnAddress("TEST");
-		product.setProdImg("tq.jpg");
-		
-		productService.addProduct(product);
-		
-		System.out.println("addProduct:" + product);
-		
+	@PostMapping("updateRecentImg")
+	public String updateRecentImg() throws Exception {
+	
 		return null;
+	}
+	
+	@GetMapping("updateProduct")
+	public String updateProductGet(@RequestParam("prodNo") int prodNo, Model model) throws Exception {
+		
+		model.addAttribute("product", productService.getProduct(prodNo));	
+	
+		return "product/updateProduct";
+	}
+	
+	//이미지여러개 어케함???
+	@PostMapping("updateProduct")
+	public String updateProductPost(@ModelAttribute("product") Product product) throws Exception {
+	
+		return null;
+	}
+	
+	@RequestMapping("updateProductCondition")
+	public String updateProductCondition(@RequestParam("prodNo") int prodNo, @RequestParam("productCondition") String productCondition) throws Exception {
+		
+		Product product = productService.getProduct(prodNo);
+		productService.getProduct(prodNo).setProdCondition(productCondition);
+		
+		productService.updateProduct(product);
+	
+		return "redirect: /storage/listStorageForAdmin";
+	}
+	
+	@GetMapping("getProduct")
+	public String getProduct(@RequestParam("prodNo") int prodNo, Model model) throws Exception {
+		
+		Product product = productService.getProduct(prodNo);
+		
+		model.addAttribute("product", product);
+		
+		return "product/getProduct";
+	}
+	
+	@RequestMapping("listProduct")
+	public String listProduct(@ModelAttribute("search") Search search, Model model) throws Exception {
+		
+		if(search.getCurrentPage()==0) {
+			search.setCurrentPage(1);
+		}
+		search.setPageSize(pageSize);
+		
+		Map<String, Object> map = productService.getProductList(search);
+		
+		Page resultPage = new Page( search.getCurrentPage(), ((Integer)map.get("totalCount")).intValue(), pageUnit, pageSize);
+		
+		model.addAttribute("list", map.get("list"));
+		model.addAttribute("resultPage", resultPage);
+		model.addAttribute("search", search);
+		
+		return "product/listProduct";
 	}
 
 }
--- Bit-forRest/src/main/java/com/mvc/forrest/controller/storage/StorageController.java
+++ Bit-forRest/src/main/java/com/mvc/forrest/controller/storage/StorageController.java
@@ -12,11 +12,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mvc.forrest.service.domain.User;
+import com.mvc.forrest.service.product.ProductService;
 import com.mvc.forrest.service.storage.StorageService;
+import com.mvc.forrest.service.user.UserService;
 
 
 
@@ -29,13 +33,67 @@ public class StorageController {
 	@Autowired
 	public StorageService storageService ;
 	
-	//@GetMapping("/addProductTest")
-	public String addProductTest() {
-		return "storage/addStorage";
+	@Autowired
+	public ProductService productService ;
+	
+	@Autowired
+	public UserService userService;
+	
+	public StorageController() {
+		System.out.println(this.getClass());
 	}
 	
-	//@RequestMapping("addProduct")
-	public String addProduct() throws Exception {
+	//보관 메인화면 단순 네비게이션
+	@GetMapping("storageMain")
+	public String storageMain(HttpSession session) {
+		
+		String userId = ((User) session.getAttribute("user")).getUserId();
+		
+				
+		
+		return "storage/storageMain";
+	}
+	
+	@GetMapping("addStorage")
+	public String addStorageGet() {
+		
+		
+		
+		return null;
+	}
+	
+	@PostMapping("addStorage")
+	public String addStoragePost() {
+		
+		return null;
+	}
+	
+	@RequestMapping("listStorage")
+	public String listStorage() {
+		
+		return null;
+	}
+	
+	@RequestMapping("listStorageForAdmin")
+	public String listStorageForAdmin() {
+		
+		return null;
+	}
+		
+	@GetMapping("extendStorage")
+	public String extendStorageGet() {
+		
+		return null;
+	}
+	
+	@PostMapping("extendStorage")
+	public String extendStoragePost() {
+		
+		return null;
+	}
+	
+	@GetMapping("getStorage")
+	public String getStorage() {
 		
 		return null;
 	}
