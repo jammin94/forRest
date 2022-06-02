477987a52e39b6c89396439cce77f6404224de9f
qwerty5266 <bitcamp@DESKTOP-QJQ7T9I>
2022-06-02 10:36:16 +0900

    0602 1035 domain,mapper 수정

--- Bit-forRest/src/main/java/com/mvc/forrest/service/domain/Old.java
+++ Bit-forRest/src/main/java/com/mvc/forrest/service/domain/Old.java
@@ -15,69 +15,7 @@ public class Old {
 	private String category;
 	private short oldState;
 	private String oldImg;
+	private String userId;
 
 
-
-	public int getOldNo() {
-		return oldNo;
-	}
-	public void setOldNo(int oldNo) {
-		this.oldNo = oldNo;
-	}
-	public int getOldPrice() {
-		return oldPrice;
-	}
-	public void setOldPrice(int oldPrice) {
-		this.oldPrice = oldPrice;
-	}
-	public String getOldTitle() {
-		return oldTitle;
-	}
-	public void setOldTitle(String oldTitle) {
-		this.oldTitle = oldTitle;
-	}
-	public String getOldDetail() {
-		return oldDetail;
-	}
-	public void setOldDetail(String oldDetail) {
-		this.oldDetail = oldDetail;
-	}
-	public Timestamp getOldDate() {
-		return oldDate;
-	}
-	public void setOldDate(Timestamp oldDate) {
-		this.oldDate = oldDate;
-	}
-	public int getOldView() {
-		return oldView;
-	}
-	public void setOldView(int oldView) {
-		this.oldView = oldView;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public void setCategory(String category) {
-		this.category = category;
-	}
-	public short getOldState() {
-		return oldState;
-	}
-	public void setOldState(short oldState) {
-		this.oldState = oldState;
-	}
-	
-
-	public String getOldImg() {
-		return oldImg;
-	}
-	public void setOldImg(String oldImg) {
-		this.oldImg = oldImg;
-	}
-	@Override
-	public String toString() {
-		return "Old  : [oldTitle]" + oldTitle
-				+ "[oldDate]" + oldDate + "[oldPrice]" +oldPrice
-				+ "[oldView]" + oldView + "[category]" +category
-				+ "[oldState]" + oldState;}
 	}
