55ddc4a4e24a5e907a5cec5e53c3fb949a0988a7
jj3031 <blitzace89@gmail.com>
2022-06-03 20:03:42 +0900

    0603 2003 정태영 firebase 수정중

--- Bit-forRest/src/test/java/com/mvc/forrest/OldServiceTest.java
+++ Bit-forRest/src/test/java/com/mvc/forrest/OldServiceTest.java
@@ -1,11 +1,12 @@
 package com.mvc.forrest;
 
 
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Map;
 
-import org.junit.Assert;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
