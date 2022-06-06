af0a25c958d0470e4d584c3b9de244c7ed4bc4f4
blitz <blitz@121.129.34.54>
2022-06-06 21:41:54 +0900

    0606 2141	 정태영 html 수정중

--- Bit-forRest/src/main/resources/static/js/faqmain.js
+++ Bit-forRest/src/main/resources/static/js/faqmain.js
@@ -1,43 +1,43 @@
 (function(){
-	// FAQ Template - by CodyHouse.co
-  var FaqTemplate = function(element) {
-		this.element = element;
-		this.sections = this.element.getElementsByClassName('cd-faq__group');
-		this.triggers = this.element.getElementsByClassName('cd-faq__trigger');
-		this.faqContainer = this.element.getElementsByClassName('cd-faq__items')[0];
-		this.faqsCategoriesContainer = this.element.getElementsByClassName('cd-faq__categories')[0];
-		this.faqsCategories = this.faqsCategoriesContainer.getElementsByClassName('cd-faq__category');
-  	this.faqOverlay = this.element.getElementsByClassName('cd-faq__overlay')[0];
-  	this.faqClose = this.element.getElementsByClassName('cd-faq__close-panel')[0];
-  	this.scrolling = false;
-  	initFaqEvents(this);
-  };
+	  // FAQ Template - by CodyHouse.co
+	  var FaqTemplate = function(element) {
+			this.element = element;
+			this.sections = this.element.getElementsByClassName('cd-faq__group');
+			this.triggers = this.element.getElementsByClassName('cd-faq__trigger');
+			this.faqContainer = this.element.getElementsByClassName('cd-faq__items')[0];
+			this.faqsCategoriesContainer = this.element.getElementsByClassName('cd-faq__categories')[0];
+			this.faqsCategories = this.faqsCategoriesContainer.getElementsByClassName('cd-faq__category');
+	  	this.faqOverlay = this.element.getElementsByClassName('cd-faq__overlay')[0];
+	  	this.faqClose = this.element.getElementsByClassName('cd-faq__close-panel')[0];
+	  	this.scrolling = false;
+	  	initFaqEvents(this);
+	  };
 
-  function initFaqEvents(faqs) {
-  	// click on a faq category
-		faqs.faqsCategoriesContainer.addEventListener('click', function(event){
-			var category = event.target.closest('.cd-faq__category');
-			if(!category) return;
-			var mq = getMq(faqs),
-				selectedCategory = category.getAttribute('href').replace('#', '');
-			if(mq == 'mobile') { // on mobile, open faq panel
-				event.preventDefault();
-				faqs.faqContainer.scrollTop = 0;
-				Util.addClass(faqs.faqContainer, 'cd-faq__items--slide-in');
-				Util.addClass(faqs.faqClose, 'cd-faq__close-panel--move-left');
-				Util.addClass(faqs.faqOverlay, 'cd-faq__overlay--is-visible');
-				var selectedSection = faqs.faqContainer.getElementsByClassName('cd-faq__group--selected');
-				if(selectedSection.length > 0) {
-					Util.removeClass(selectedSection[0], 'cd-faq__group--selected');
+	  function initFaqEvents(faqs) {
+	  	// click on a faq category
+			faqs.faqsCategoriesContainer.addEventListener('click', function(event){
+				var category = event.target.closest('.cd-faq__category');
+				if(!category) return;
+				var mq = getMq(faqs),
+					selectedCategory = category.getAttribute('href').replace('#', '');
+				if(mq == 'mobile') { // on mobile, open faq panel
+					event.preventDefault();
+					faqs.faqContainer.scrollTop = 0;
+					Util.addClass(faqs.faqContainer, 'cd-faq__items--slide-in');
+					Util.addClass(faqs.faqClose, 'cd-faq__close-panel--move-left');
+					Util.addClass(faqs.faqOverlay, 'cd-faq__overlay--is-visible');
+					var selectedSection = faqs.faqContainer.getElementsByClassName('cd-faq__group--selected');
+					if(selectedSection.length > 0) {
+						Util.removeClass(selectedSection[0], 'cd-faq__group--selected');
+					}
+					Util.addClass(document.getElementById(selectedCategory), 'cd-faq__group--selected');
+				} else { // on desktop, scroll to section
+					if(!window.requestAnimationFrame) return;
+					event.preventDefault();
+					var windowScrollTop = window.scrollY || document.documentElement.scrollTop;
+					Util.scrollTo(document.getElementById(selectedCategory).getBoundingClientRect().top + windowScrollTop + 2, 200);
 				}
-				Util.addClass(document.getElementById(selectedCategory), 'cd-faq__group--selected');
-			} else { // on desktop, scroll to section
-				if(!window.requestAnimationFrame) return;
-				event.preventDefault();
-				var windowScrollTop = window.scrollY || document.documentElement.scrollTop;
-				Util.scrollTo(document.getElementById(selectedCategory).getBoundingClientRect().top + windowScrollTop + 2, 200);
-			}
-		});
+			});
 
 		// on mobile -> close faq panel
 		faqs.faqOverlay.addEventListener('click', function(event){
