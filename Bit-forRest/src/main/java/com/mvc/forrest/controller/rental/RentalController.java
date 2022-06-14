85e281d92b8290b5bc0cac5fb08007e11a43602d
hyeongsan <sanstory12rt@naver.com>
2022-06-14 20:36:43 +0900

    0614 2036 hyeongsan/ addRental

--- Bit-forRest/src/main/java/com/mvc/forrest/controller/rental/RentalController.java
+++ Bit-forRest/src/main/java/com/mvc/forrest/controller/rental/RentalController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mvc.forrest.config.auth.LoginUser;
 import com.mvc.forrest.service.coupon.CouponService;
 import com.mvc.forrest.service.domain.Coupon;
 import com.mvc.forrest.service.domain.Page;
@@ -70,11 +72,18 @@ public class RentalController {
 		//prodNo를 받아서 productService.getProduct 로 product객체를 받고, html에서 product.~~로 페이지내에서 사용
 		Product product = productService.getProduct(prodNo);		
 		
-		//쿠폰받기
-		Map<String,Object> mapCoupon = couponService.getCouponList(search);
+		//암호화된 유저아이디를 받아옴
+		LoginUser loginUser= (LoginUser)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String userId= loginUser.getUser().getUserId();
+		System.out.println("userId: "+userId);
+		
+		//회원의 보유쿠폰리스트를 받아옴
+		Map<String,Object> map =couponService.getOwnCouponList(userId);
+		
+		System.out.println("쿠폰쿠폰"+map.get("list"));
 		
 		model.addAttribute("product",product);
-		model.addAttribute("coupon",mapCoupon.get("list"));
+		model.addAttribute("list",map.get("list"));
 		
 		return "rental/addRental";
 	}
@@ -84,8 +93,8 @@ public class RentalController {
 	@PostMapping("addRental")
 	public String addRental(@ModelAttribute("rental") Rental rental, Model model ) throws Exception {
 		
-		Product product = null;
-		product = productService.getProduct(rental.getProdNo());	
+	//	Product product = null;
+	//	product = productService.getProduct(rental.getProdNo());	
 //		userService.getUser(rental.getUserId());	  ( 대기 )	
 		
 		//0. i'm port에서 나온 값 + 화면상 입력값들 ModelAttribute("rental")에 담겨있음.
@@ -95,7 +104,7 @@ public class RentalController {
 		
 		//2. getRental에서 쓰기위해 model을 통해 전달
 		model.addAttribute("rental",rental);
-		model.addAttribute("product",product);
+//		model.addAttribute("product",product);
 //		model.addAttribute("user",user);
 		
 		//3. getRental.jsp 에서 model들 다 뽑아쓰면됨
