bf2734cf7bb19c9da884feee0ba3b0c216cf8a7c
goosebeomsu <captain9697@naver.com>
2022-06-14 19:41:05 +0900

    0614 1940 goosebeomsu: getProduct 하단 리뷰 작성

--- Bit-forRest/src/main/java/com/mvc/forrest/service/domain/RentalReview.java
+++ Bit-forRest/src/main/java/com/mvc/forrest/service/domain/RentalReview.java
@@ -1,6 +1,7 @@
 package com.mvc.forrest.service.domain;
 
-import java.util.Date;
+
+import java.sql.Date;
 
 import lombok.Data;
 
@@ -10,8 +11,8 @@ public class RentalReview {
 	private int reviewNo;
 	private String reviewImg;
 	private String reviewDetail;
-	private String reviewScore;
-	private int prodNo;
+	private int reviewScore;
+	private String prodNo;
 	private String userId;
 	private Date regDate;
 	private User user;
