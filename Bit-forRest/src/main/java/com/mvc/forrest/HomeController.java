97b4d32af31a7b8664c87dcda6a54e6c14781c86
jj3031 <blitzace89@gmail.com>
2022-06-17 10:27:06 +0900

    0617 1026 정태영 로그인 설정 세션 추가

--- Bit-forRest/src/main/java/com/mvc/forrest/HomeController.java
+++ Bit-forRest/src/main/java/com/mvc/forrest/HomeController.java
@@ -3,12 +3,16 @@ package com.mvc.forrest;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.mvc.forrest.config.auth.LoginUser;
 import com.mvc.forrest.service.domain.Old;
 import com.mvc.forrest.service.domain.Product;
 import com.mvc.forrest.service.old.OldService;
@@ -26,6 +30,21 @@ public class HomeController {
 	@GetMapping("/")
 	public String room(Model model) throws Exception {
 		
+		List<Old> listOld = oldService.getOldListForIndex();
+		List<Product> listProduct = productService.getProductListForIndex();
+		model.addAttribute("listOld", listOld);
+		model.addAttribute("listProd", listProduct);
+		
+		return "main/index";
+	}
+	
+	@GetMapping("/afterLogin")
+	public String afterLogin(Model model, HttpSession session) throws Exception {
+		
+		LoginUser loginUser= (LoginUser)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String userId= loginUser.getUser().getUserId();
+		session.setAttribute("userId", userId);
+		session.setMaxInactiveInterval(-1);
 		List<Old> listOld = oldService.getOldListForIndex();
 		List<Product> listProduct = productService.getProductListForIndex();
 		model.addAttribute("listOld", listOld);
