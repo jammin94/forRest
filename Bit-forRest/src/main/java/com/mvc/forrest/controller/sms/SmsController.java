abf1f9e259a4715b26f158236815491e1378a516
lsm45 <lsm45@DESKTOP-TMLFMUP>
2022-06-18 23:25:19 +0900

    0618 1124 상민 401..

---
+++ Bit-forRest/src/main/java/com/mvc/forrest/controller/sms/SmsController.java
@@ -0,0 +1,33 @@
+package com.mvc.forrest.controller.sms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.google.auto.value.AutoAnnotation;
+import com.mvc.forrest.service.sms.SmsService;
+
+@Controller
+@RequestMapping("/sms/*")
+public class SmsController {
+
+	@Autowired
+	SmsService smsService;
+	
+	@RequestMapping("sendSMS")
+	public String sendSMS(@RequestParam String phone) throws Exception {
+		
+		String timestamp = Long.toString(System.currentTimeMillis());
+		System.out.println("컨트롤러 타임스탬프 : "+timestamp);
+		String signature= smsService.makeSignature(timestamp);
+		System.out.println("컨트롤러 시그니처 : "+signature);
+		smsService.makeMassage(signature, timestamp, phone);
+		System.out.println("sendSMS END");
+		
+		return "user/addUser";
+	}
+	
+}
