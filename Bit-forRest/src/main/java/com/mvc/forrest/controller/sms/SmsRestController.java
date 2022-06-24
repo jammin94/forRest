9dfd62c34a16594d55288229f51edd0971c0d88b
bitcamp <bitcamp@bitcamp.com>
2022-06-24 11:41:54 +0900

    conflict 해결

--- Bit-forRest/src/main/java/com/mvc/forrest/controller/sms/SmsRestController.java
+++ Bit-forRest/src/main/java/com/mvc/forrest/controller/sms/SmsRestController.java
@@ -27,12 +27,9 @@ public class SmsRestController {
     	
 		Random rd = new Random();
 		int authNum = rd.nextInt(888888)+111111;
-		
-<<<<<<< HEAD
+
 	smsService.makeMassage(authNum, phone);
-=======
-		smsService.makeMassage(authNum, phone);
->>>>>>> refs/remotes/origin/master
+
 		
 		return authNum;
 	}
