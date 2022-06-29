915342d21452161277fef46210d642ffe0a0a8cc
jj3031 <blitzace89@gmail.com>
2022-06-29 10:18:22 +0900

    진짜 마지막 푸시

--- Bit-forRest/src/main/java/com/mvc/forrest/config/SecurityConfig.java
+++ Bit-forRest/src/main/java/com/mvc/forrest/config/SecurityConfig.java
@@ -15,8 +15,8 @@ import lombok.RequiredArgsConstructor;
 
 
 @RequiredArgsConstructor
-@EnableWebSecurity//해당 파일로 시큐리티를 활성화
-@Configuration//IoC
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired private PrincipalOauth2UserService principalOauth2UserService;
 	@Autowired private LoginFailureHandler loginFailureHandler;
@@ -29,9 +29,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		System.out.println("configure 작동!");
-		//super 삭제 - 기존 시큐리티가 가지고 있는 기능이 없어짐 (Overriding)
-		http.csrf().disable();//csrf 토큰을 사용하지 않겠다.
-		http.authorizeHttpRequests()  //인가 요청이 오면
+		
+		http.csrf().disable();
+		http.authorizeHttpRequests()  
 			.antMatchers("/user/manager","/product/getProduct","/storage/listStorage", "/storage/extendStorage","/old/addOldReport","/old/updateOldState","/old/addOld","/old/updateOld","/old/listOldMine","/old/deleteOld", "/storage/getStorage","http://192.168.0.42:3001/oldChat/**").authenticated()
 			.antMatchers("/user/getUser", "/oldLike/addOldLike", "/user/deleteUser", "/old/listOldAfterLogin", "/oldLike/deleteOldLikeOnList" , "/storage/addStorage", "/produnct/listProductAfterLogin").authenticated()
 			.antMatchers("/storage/listStorageForAdmin","/board/addAnnounce","/board/updateAnnounce","/board/deleteAnnounce").authenticated()
@@ -40,18 +40,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.formLogin()
 			.usernameParameter("userId")
 			.passwordParameter("password")
-			.loginPage("/user/login") //GET
-			.loginProcessingUrl("/user/login") //POST
+			.loginPage("/user/login")
+			.loginProcessingUrl("/user/login") 
 			.defaultSuccessUrl("/user/afterLogin")
 			.failureHandler(loginFailureHandler)
 			.and()
-			.logout()// 로그인과 로그아웃은 csrf 사용시 사용 안할 때는 로그아웃은 get방식도 가능
+			.logout()
 			.invalidateHttpSession(true)
 			.logoutSuccessUrl("/").and()			
 			.oauth2Login().loginPage("/user/login")
 			.failureUrl("/user/login")
 			.userInfoEndpoint()
 			.userService(principalOauth2UserService);
+		
+		
+		
+		
+		
+		
 			//1. antMatchers 안에 있는 주소에 접속 할 때는 인증이 필요하다
 			//2. 그 외 모든 리퀘스트는 다 승인한다
 			//3. 그리고
--- Bit-forRest/src/main/java/com/mvc/forrest/config/auth/LoginUser.java
+++ Bit-forRest/src/main/java/com/mvc/forrest/config/auth/LoginUser.java
@@ -38,54 +38,6 @@ public class LoginUser  implements UserDetails , OAuth2User{
 	public LoginUser(User user){
 		System.out.println(user);
 		this.user=user;
-		///////////////////////////////////////////////////////////////
-//		
-//		try {
-//	        	System.out.println(":: Connect to Chatting Service");
-//			String reqURL = "http://192.168.0.42:3001/sessionLoginLogout/login/"+user.getUserId();
-//			URL url = new URL(reqURL);
-//			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//			conn.setRequestMethod("GET");
-//			conn.setConnectTimeout(500);
-//			int responseCode = conn.getResponseCode();
-//			System.out.println(":: Chatting Service responseCode : " + responseCode);
-//	        	System.out.println("Node server is Dead ..");
-//		}catch(Exception e){
-//			System.out.println("채팅연결 실패");
-//		}
-//		
-//		try {
-//	        if(user.getJoinDate().equals(user.getRecentDate())) {
-//		        OwnCoupon oc = new OwnCoupon();
-//				Coupon coupon = couponService.getCoupon("2");	//2번 쿠폰 = 신규회원 쿠폰
-//				Calendar cal= Calendar.getInstance();
-//				cal.add(Calendar.DATE,30);
-//				Timestamp ts1 = new Timestamp(System.currentTimeMillis());
-//				Timestamp ts2 = new Timestamp(cal.getTimeInMillis());
-//				oc.setOwnUser(user);
-//				oc.setOwnCoupon(coupon);
-//				oc.setOwnCouponCreDate(ts1);
-//				oc.setOwnCouponDelDate(ts2);
-//				couponService.addOwnCoupon(oc);
-//				
-//				System.out.println("신규유저 쿠폰발급");
-//	        }
-//		}catch(Exception e) {
-////			e.printStackTrace();
-//			System.out.println("신규유저 쿠폰발급 실패");
-//		}
-//		
-//        try {
-//        	System.out.println(user);
-//			System.out.println(user.getUserId());
-//			userService.updateRecentDate(user);
-//			
-//		} catch (Exception e) {
-////			e.printStackTrace();
-//			System.out.println("접속날짜 갱신 실패");
-//		}	
-//        
-		////////////////////////////////////////////////////////////////
 	}
 	
     public LoginUser(User user, Map<String, Object> attribute) {
